6616c7964ce3195a6a2f545157327c2995699817
rksgr <r_kjha@yahoo.com>
2021-12-14 11:30:09 +0530

    Added: Welcome message

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.dec_13_empwagecomput_oops;
+
+public class Welcome {
+    public static void main(String []args){
+       System.out.println("Welcome to Employee Wage Computation Program on "
+            + "Master Branch!");
+    }
+}
