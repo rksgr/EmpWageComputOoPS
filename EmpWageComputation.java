3065c2496834e7dd5e8d9ac458edff6e17c810c5
rksgr <r_kjha@yahoo.com>
2021-12-14 13:39:25 +0530

    Refactored: Calculate monthly wage of employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -68,9 +68,21 @@ public class EmpWageComputation {
         }
         return emp_daily_wage;
     }
+    /*
+    Use Case 5: Calculate wages for a month
+    */
+    public double empMonthlyWage(){
+        double emp_month_wage = 0;
+        for (int i=1;i<31;i++){
+            // call the method which computes daily wage based on employee's hours worked
+            emp_month_wage += empDailyWageSwitchCase();
+        }
+        return emp_month_wage;
+    }
+    // Main method
     public static void main(String []args){
         EmpWageComputation ewc = new EmpWageComputation();
-        double emp_daily_wage = ewc.empDailyWageSwitchCase();
-        System.out.println("The daily wage of the employee = " + emp_daily_wage);
+        double emp_monthly_wage = ewc.empMonthlyWage();
+        System.out.println("The monthly wage of the employee = " + emp_monthly_wage);
     }
 }
