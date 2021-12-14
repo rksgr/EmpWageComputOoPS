aeff0be98ef31c07c36a7b40e8862069ab2da88f
rksgr <r_kjha@yahoo.com>
2021-12-14 12:32:35 +0530

    Refactored: Employee wage if part time

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -16,15 +16,27 @@ public class EmpWageComputation {
     /*
     Use case 2: Calculate Daily Employee Wage
     */
-    public int empDailyWage(){
+    public double empDailyWage(){
         int wage_per_hour = 20;
         int full_day_hour = 8;
-        int emp_daily_wage = wage_per_hour*full_day_hour;
+        double emp_daily_wage = wage_per_hour*full_day_hour;
         return emp_daily_wage;
+    }
+    /*
+    Use case 3: Calculate wage of employee in case of part time
+                Method overloading
+    */
+    public double empDailyWage(boolean ifPartTime){
+        double part_time_wage = 0;
+        // Part time he works 4 hours in a day, call empDailyWage() method
+        if (ifPartTime){
+            part_time_wage = 0.5*empDailyWage();
+        }
+        return part_time_wage;
     }
     public static void main(String []args){
         EmpWageComputation ewc = new EmpWageComputation();
-        int emp_daily_wage = ewc.empDailyWage();
-        System.out.println("The daily wage of the employee = " + emp_daily_wage);
+        double emp_part_time_wage = ewc.empDailyWage(true);
+        System.out.println("The part time wage of the employee = " + emp_part_time_wage);
     }
 }
