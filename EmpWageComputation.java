ba03c98206a5a421510e90a5b157d49d8feb78a6
rksgr <r_kjha@yahoo.com>
2021-12-14 12:08:57 +0530

    Refactored: Calculate daily Employee wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,6 +2,7 @@
 package com.mycompany.dec_13_empwagecomput_oops;
 
 public class EmpWageComputation {
+    
     /*
     Use case 1: Check whether employee is present
     */
@@ -12,10 +13,18 @@ public class EmpWageComputation {
         }
         return emp_pres;
     }
+    /*
+    Use case 2: Calculate Daily Employee Wage
+    */
+    public int empDailyWage(){
+        int wage_per_hour = 20;
+        int full_day_hour = 8;
+        int emp_daily_wage = wage_per_hour*full_day_hour;
+        return emp_daily_wage;
+    }
     public static void main(String []args){
         EmpWageComputation ewc = new EmpWageComputation();
-        boolean emp_pres = ewc.empIsPresent();
-        if (emp_pres){ System.out.println("Employee is present");
-        } else { System.out.println("Employee is absent");}
+        int emp_daily_wage = ewc.empDailyWage();
+        System.out.println("The daily wage of the employee = " + emp_daily_wage);
     }
 }
